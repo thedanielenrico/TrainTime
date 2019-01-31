2530c9217d39e7d8efe583e5bf5c9bc819779a9b
thedanielenrico <thedanielenrico@gmail.com>
2019-01-31 09:38:39 -0800

    Put firebase database in

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,46 @@
-var $tableBody = $("#tableBody");
-var trainArray = [
-    {
-        name: "Hogwarts Express",
-        destination: "Hogwarts",
-        frequency: 30,
-        arrivalTime: 0934,
-        minAway: 5,
-    },
-    {
-        name: "Caboose",
-        destination: "Funky Town",
-        frequency: 10,
-        arrivalTime: 1100,
-        minAway: 10,
-    }
-];
+var tableBody = $("#tableBody");
 
-for (var i = 0; i < trainArray.length; i++) {
-    var $tRow = $("<tr>")
-    $tRow.append($(`<td>${trainArray[i].name}</td>`))
-    $tRow.append($(`<td>${trainArray[i].destination}</td>`))
-    $tRow.append($(`<td>${trainArray[i].frequency}</td>`))
-    $tRow.append($(`<td>${trainArray[i].arrivalTime}</td>`))
-    $tRow.append($(`<td>${trainArray[i].minAway}</td>`))
-    $tableBody.append($tRow);
-}
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDjgQ0_dtaVDYEKHLwvyaryxXzOxZ8Gpu0",
+    authDomain: "traintime-ucd.firebaseapp.com",
+    databaseURL: "https://traintime-ucd.firebaseio.com",
+    projectId: "traintime-ucd",
+    storageBucket: "traintime-ucd.appspot.com",
+    messagingSenderId: "865325209308"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$("#addTrainButton").on("click", function () {
+    var name = $("#trainName").val().trim();
+    var destination = $("#destination").val().trim();
+    var firstTrain = $("#firstTrainTime").val().trim();
+    var frequency = $("#frequency").val().trim();
+
+    database.ref().push({
+        name: name,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+    })
+})
+
+database.ref().on("child_added",
+    function (snapshot) {
+        var dbObj = snapshot.val();
+        var $tRow = $("<tr>")
+        $tRow.append($(`<td>${dbObj.name}</td>`))
+        $tRow.append($(`<td>${dbObj.destination}</td>`))
+        $tRow.append($(`<td>${dbObj.frequency}</td>`))
+        $tRow.append($(`<td>${dbObj.arrivalTime}</td>`))
+        $tRow.append($(`<td>${dbObj.minAway}</td>`))
+        tableBody.append($tRow);
+    },
+    function (error) {
+        console.log(error);
+    }
+)
